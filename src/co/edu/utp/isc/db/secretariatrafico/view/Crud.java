16cb47e7155450781d32a3085adda43ec281a387
Esteban <estebangsan13@gmail.com>
2019-07-16 08:10:32 -0500

    Agregados más métodos

--- src/co/edu/utp/isc/db/secretariatrafico/view/Crud.java
+++ src/co/edu/utp/isc/db/secretariatrafico/view/Crud.java
@@ -1,5 +1,6 @@
 package co.edu.utp.isc.db.secretariatrafico.view;
 
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -9,8 +10,20 @@ import javax.swing.table.DefaultTableModel;
 public interface Crud {
     
     DefaultTableModel getModelo();
+    
+    void registrar();
+    
     void cargarRegistros();
+    
     void guardar();
+    
+    void seleccionar(JTable tblTabla);
+    
+    void actualizar();
+    
+    void limpiarCampos();
+    
+    void eliminar(JTable tblTabla);
     
 //    void crearTabla();
     
